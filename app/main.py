a6bb9f02a2ae43e6624514c9f6cfc15bce2c06b0
rakesh.sai <rakesh.sai@pehchaan.me>
2025-12-11 09:44:21 +0530

    cors update

--- app/main.py
+++ app/main.py
@@ -55,7 +55,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173","https://vectatech.com.au"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
